28f5d69bd20b6e5058a0a5cf09c604540ed1e72b
Noman Aijaz <noman.aijaz332@gmail.com>
2022-11-09 01:59:37 +0500

    add a single file that contain main header and included in all files using extends in pug

--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const router  = express.Router();
-const path = require('path');
 
 const products = [];
 router.get('/add-Item',(req,res, next)=>{
-    res.render('add-Items',{pageTitle:"Add Products"});
+    res.render('add-Items',{pageTitle:"Add Products",path:"/admin/add-Item"});
 })
 router.post('/add-Item',(req, res)=>{
     products.push({title: req.body.title})
